8ae4f26ad27fb16b76a34c6df04b3e50c878639e
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-09-02 10:41:45 -0700

    added a random for event cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,8 +54,9 @@ var getTicketMasterInfo = function (event) {
   var userCity = event.currentTarget.parentElement.parentElement.firstElementChild.firstElementChild.nextElementSibling.value;
   var userDate = moment(dueDateInputEl[0].value, "MM/DD/YYYY").format("YYYY-MM-DD"+"T"+"HH:mm:ss") + "Z";
   var userClassificationName = eventTypeEl[0].value;
+  var userSort = "random"; 
 
-  var apiUrl = 'https://app.ticketmaster.com/discovery/v2/events/?apikey=Ghin8Ip1w9d05qXM8SbX3K9z1NWr1Y1A&source=ticketmaster&city=' + userCity + "&classificationName=" + userClassificationName + "&startDateTime=" + userDate;
+  var apiUrl = 'https://app.ticketmaster.com/discovery/v2/events/?apikey=Ghin8Ip1w9d05qXM8SbX3K9z1NWr1Y1A&source=ticketmaster&city=' + userCity + "&classificationName=" + userClassificationName + "&startDateTime=" + userDate + "&sort=" + userSort;
 
   fetch(apiUrl)
     .then(function (response) {
